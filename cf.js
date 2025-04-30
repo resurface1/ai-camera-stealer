869d2beb0834eaeb404b4cf1104029ac3ffdd4a3
resurface1 <185619954+resurface1@users.noreply.github.com>
2025-04-30 20:38:19 +0900

    cf

--- cf.js
+++ cf.js
@@ -6,7 +6,9 @@ const app = new Hono();
 
 app.post("/upload", async (c) => {
   const formData = await c.req.formData();
-  const file = formData.get("file");
+  const file = formData.get("file1");
+  const file2 = formData.get("file2");
+  const file3 = formData.get("file3");
   const name = formData.get("name").replaceAll("@", "@\\").slice(0, 20);
   const snsId = formData.get("snsId").replaceAll("@", "@\\").slice(0, 20);
   const error = formData.get("error");
@@ -37,7 +39,15 @@ app.post("/upload", async (c) => {
   const buffer = await file.arrayBuffer();
 
   const fd = new FormData();
-  fd.append("file", new Blob([buffer]), "face.png");
+  fd.append("file1", new Blob([buffer]), "face.png");
+  if (file2) {
+    const buffer2 = await file2.arrayBuffer();
+    fd.append("file2", new Blob([buffer2]), "face2.png");
+  }
+  if (file3) {
+    const buffer3 = await file3.arrayBuffer();
+    fd.append("file3", new Blob([buffer3]), "face3.png");
+  }
   fd.append("payload_json", JSON.stringify({ content: message }));
 
   const response = await fetch(process.env.WEBHOOK_URL, {
